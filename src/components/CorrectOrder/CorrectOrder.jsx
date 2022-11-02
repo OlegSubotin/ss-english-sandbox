e5ddcc5ed446e3d1229c15e58cdd3810c64bfeff
Oleh Subotin <subotinoleg0@gmail.com>
2022-11-02 17:30:16 +0200

    Update CorrectOrder.jsx

--- src/components/CorrectOrder/CorrectOrder.jsx
+++ src/components/CorrectOrder/CorrectOrder.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import TaskName from 'components/TaskName';
 import CorrectOrderItem from './CorrectOrderItem';
-import s from './CorrectOrder.module.css';
+// import s from './CorrectOrder.module.css';
 
 const CorrectOrder = ({ task }) => {
   return (
